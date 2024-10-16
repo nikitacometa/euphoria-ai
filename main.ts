5cce474d4b66273c32aed6d6aef15abbc6699bba
wackloner <wackloner@gmail.com>
2024-10-17 03:11:43 +0700

    add basic bot commands

--- main.ts
+++
@@ -1,8 +0,0 @@
-import { Bot } from 'grammy'
-import { TELEGRAM_API_TOKEN } from './config'
-
-const bot = new Bot(TELEGRAM_API_TOKEN)
-
-bot.on('message:text', ctx => ctx.reply(ctx.message.text))
-
-bot.start()
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const TELEGRAM_API_TOKEN = process.env.TELEGRAM_API_TOKEN as string;
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY as string;
+export const GPT_VERSION = process.env.GPT_VERSION as string;
